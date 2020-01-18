bfdc04ca7b9eafbf76ddf63d491808aed451bb73
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-18 06:40:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_the_cheese(array)# code an argument here (array of strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.each{|ingredient| if cheese_types.include?(ingredient) 
-          do ingredient}
+           ingredient}
     
     
 end
