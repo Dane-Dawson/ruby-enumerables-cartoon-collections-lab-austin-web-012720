0b4a39652e5d1d761994d30fb860da8ef9117b21
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-18 05:21:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def roll_call_dwarves(array)# code an argument here
 end
 
 def summon_captain_planet(array)# code an argument here
-  capitalize = array.map! { |planet_call| planet_call.capitalize }
-  excited_cries = capitalize.each { |planet_call_exclaim| push planet_call_exclaim + "!" }
+  capitalize = []
+  array.map! { |planet_call| planet_call.capitalize }
+  excited_cries = []
+  capitalize.each { |planet_call_exclaim| excited_cries.push planet_call_exclaim + "!" }
   excited_cries
   
   #capitalize each element and add an exclamation point, return in an array
