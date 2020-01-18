b7dd56ca803e9c261f9ea025a2e9717cbbb7f140
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-18 05:13:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # should print out each name, in number order, using puts. use each_with_index method
-hash = Hash.new
+hash = {}
  array.each_with_index { |dwarf, index| hash = dwarf[index]}
 
 puts hash  
