e5a3beaea773b2d5c27f143a49e25b1f2658bd68
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-18 05:22:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)# code an argument here
   capitalize = []
   array.map! { |planet_call| planet_call.capitalize }
   excited_cries = []
-  capitalize.each { |planet_call_exclaim| excited_cries.push planet_call_exclaim + "!" }
+  capitalize.each { |planet_call_exclaim| excited_cries.push "#{planet_call_exclaim} !" }
   puts excited_cries
   
   #capitalize each element and add an exclamation point, return in an array
